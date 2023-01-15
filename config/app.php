d216980d182236d948fb89e813e2d1b8229adb58
irfanwafiy <irf.1499@gmail.com>
2023-01-15 21:58:43 +0800

    Revert "Tambah model baru dan tambah package chart"
    This reverts commit 4dff2f3c4aa915f742cf8ddade19fb0f96ef7ee6.

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        ConsoleTVs\Charts\ChartsServiceProvider::class,
 
         /*
          * Package Service Providers...
